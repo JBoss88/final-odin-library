a10e85d9dac46032dbec1ac20785fd08a87a2df7
Jackson <jacksonboss88@gmail.com>
2024-10-06 13:40:43 -0500

    Added delete buttons, checkmark checks, and js to newBookBtn to display form

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const newBookBtn = document.querySelector('#new-book-btn');
 const submit = document.querySelector('#submit');
+const libraryContainer = document.querySelector('#library');
+const formData = document.querySelector('#form-data');
+
+newBookBtn.addEventListener('click', () => {
+    const removeMain = document.querySelector('.remove');
+    removeMain.style.display = 'block';
+});
 
 const myLibrary = [];
 
@@ -10,29 +17,45 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-
-newBookBtn.addEventListener('click', () => {
-    const formData = document.querySelector('#form-data');
-    formData.style.display = 'block';
-});
-
-function addBookToLibrary() {
+submit.addEventListener('click', () => {
+    document.querySelector('#form-data').addEventListener('submit', (event) => {
+        event.preventDefault();
+    });
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
     let read = document.querySelector('#read').checked;
-    myLibrary.push(new Book(title, author, pages, read));
-    console.log(myLibrary)
 
-}
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 
-function submitData() {
-    submit.addEventListener('click', () => {
-        addBookToLibrary()
-    });
-}
-submitData()
+    addBookToDom(newBook);
 
-document.querySelector('#form-data').addEventListener('submit', (event) => {
-    event.preventDefault();
+    function addBookToDom(book) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const titleEl = document.createElement('p');
+        titleEl.textContent =  `Title: ${book.title}`;
+
+        const authorEl = document.createElement('p');
+        authorEl.textContent = `Author: ${book.author}`;
+
+        const pagesEl = document.createElement('p');
+        pagesEl.textContent = `Pages: ${book.pages}`;
+
+        const readEl = document.createElement('p');
+        readEl.textContent = `Read: ${book.read === true ? 'Yes' : 'No'}`;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+
+        deleteBtn.addEventListener('click', () => {
+            card.textContent = '';
+        });
+
+        libraryContainer.append(card);
+        card.append(titleEl, authorEl, pagesEl, readEl, deleteBtn);
+    }
 });
